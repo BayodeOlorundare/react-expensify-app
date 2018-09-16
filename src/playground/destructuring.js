3531ce2ced4d755f181b0866a10a9aff4c6a0c80
BayodeOlorundare <bayodar3@gmail.com>
2018-09-16 01:50:05 -0400

    App Heroku push

--- src/playground/destructuring.js
+++ src/playground/destructuring.js
@@ -3,48 +3,43 @@
 //
 
 // const person = {
-//     name: 'Bayo',
-//     age: 29,
-//     location: {
-//         city: 'Toronto',
-//         temp: 20
-//     }
+//   name: 'Andrew',
+//   age: 27,
+//   location: {
+//     city: 'Philadelphia',
+//     temp: 88
+//   }
 // };
 
-//You can set up a default value if no value exist on object prop
 // const { name: firstName = 'Anonymous', age } = person;
-//const name = person.name;
-//const age = person.age;
-
-// console.log(`${firstName} is ${age}.`)
-
-//You can change the name of the variable to something local
+// console.log(`${firstName} is ${age}.`);
 
 // const { city, temp: temperature } = person.location;
-// console.log(`It's ${temperature} degrees in ${city}.`)
-
-// const book = {
-//     title: 'Ego is the Enemy',
-//     author: 'Ryan Holiday',
-//     publisher: {
-//         name: 'Penguin'
-//     }
+// if (city && temperature) {
+//   console.log(`It's ${temperature} in ${city}.`);
 // }
 
-// const { name: publisherName = 'Self-published'} = book.publisher;
-// console.log(publisherName);
+// const book = {
+//   title: 'Ego is the Enemy',
+//   author: 'Ryan Holiday',
+//   publisher: {
+//     // name: 'Penguin'
+//   }
+// };
+
+// const { name: publisherName = 'Self-Published' } = book.publisher;
+
+// console.log(publisherName); // Penguin, Self-Published
 
 //
 // Array destructuring
 //
 
-const address = ['1299 Dundas Street West', 'Toronto', 'Ontario', 'M4V 1J0'];
-
-const [ , city = 'No where', province,] = address;
-
-console.log(`You are in ${city} ${province}.`);
+// const address = ['1299 S Juniper Street', 'Philadelphia', 'Pennsylvania', '19147'];
+// const [, city, state = 'New York'] = address;
+// console.log(`You are in ${city} ${state}.`);
 
 const item = ['Coffee (iced)', '$3.00', '$3.50', '$3.75'];
+const [itemName, , mediumPrice] = item;
 
-const [ coffee, , medium] = item;
-console.log(`A medium ${coffee} costs ${medium}`)
+console.log(`A medium ${itemName} costs ${mediumPrice}`);
--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -2,74 +2,66 @@ import { createStore } from 'redux';
 
 // Action generators - functions that return action objects
 
-
-
 const incrementCount = ({ incrementBy = 1 } = {}) => ({
-    type: 'INCREMENT',
-    incrementBy
+  type: 'INCREMENT',
+  incrementBy
 });
 
 const decrementCount = ({ decrementBy = 1 } = {}) => ({
-    type: 'DECREMENT',
-    decrementBy
+  type: 'DECREMENT',
+  decrementBy
 });
 
-const setCount = ({ count = 0 } = {}) => ({
-    type: 'SET',
-    count
+const setCount = ({ count }) => ({
+  type: 'SET',
+  count
 });
 
 const resetCount = () => ({
-    type: 'RESET'
+  type: 'RESET'
 });
 
 // Reducers
-// 1. Reducers are pure functions - it's output is determined by it's input; does not interact w/ or relay on variables outside
-// 2. Never change state or action
-
+// 1. Reducers are pure functions
+// 2. Never change state or actiton
 
 const countReducer = (state = { count: 0 }, action) => {
-    switch (action.type) {
-        case 'INCREMENT':  
-        return {
-                count: state.count + action.incrementBy
-            };
-        case 'DECREMENT':
-            return {
-                count: state.count - action.decrementBy
-            };
-        case 'SET':
-            return {
-                count: action.count
-            }
-        case 'RESET':
-            return {
-                count: 0
-            };
-        default:
-            return state;
-    }
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        count: state.count + action.incrementBy
+      };
+    case 'DECREMENT':
+      return {
+        count: state.count - action.decrementBy
+      };
+    case 'SET':
+      return {
+        count: action.count
+      };
+    case 'RESET':
+      return {
+        count: 0
+      };
+    default:
+      return state;
+  }
 };
 
 const store = createStore(countReducer);
 
-// How we can see changes to the redux store
 const unsubscribe = store.subscribe(() => {
-    console.log(store.getState());
+  console.log(store.getState());
 });
 
-store.dispatch(incrementCount({ incrementBy: 5 }));
+store.dispatch(incrementCount({ incrementBy: 5 }))
 
 store.dispatch(incrementCount());
 
-// I'd like to decrement the count
+store.dispatch(resetCount());
+
 store.dispatch(decrementCount());
 
 store.dispatch(decrementCount({ decrementBy: 10 }));
 
-// I'd like to set the count
-store.dispatch(setCount());
-store.dispatch(setCount({ count: 101 }));
-
-// I'd like to reset the count
-store.dispatch(resetCount());
+store.dispatch(setCount({ count: -100 }));
